62f9b825ee18ee6329240db634181882b96ada24
prakulkumar <dhiman404prakul@gmail.com>
2019-03-20 02:42:28 +0530

    updated

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Weather from './components/weather/weather';
-import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(<Weather />, document.getElementById('root'));
